5182d8063884e8c7cea7e26da4ffd6b7d0be6d97
Chris Jantzen <jantzeca@mail.uc.edu>
2020-02-18 20:18:23 -0500

    server side set up

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+mongoose
+  .connect(`mongodb://chris:test123@ds263928.mlab.com:63928/gql-ninja`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to database...'));
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
